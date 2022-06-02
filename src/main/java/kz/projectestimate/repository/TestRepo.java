dff1c41bccd106053f8c8c7b00fae85fe54a0103
Ardakesbatyr <87215511+Ardakesbatyr@users.noreply.github.com>
2022-06-02 17:38:05 +0600

    Delete TestRepo.java

--- src/main/java/kz/projectestimate/repository/TestRepo.java
+++
@@ -1,9 +0,0 @@
-package kz.projectestimate.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import kz.projectestimate.entity.TestEntityClass;
-
-public interface TestRepo extends JpaRepository<TestEntityClass, Integer>{
-
-}
