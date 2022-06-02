cb6bf350efb0404e62cdf9e767f31bdd29f7b7b3
Ardakesbatyr <87215511+Ardakesbatyr@users.noreply.github.com>
2022-06-02 17:37:52 +0600

    Delete CrudRepository.java

--- src/main/java/kz/projectestimate/repository/CrudRepository.java
+++
@@ -1,9 +0,0 @@
-package kz.projectestimate.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import kz.projectestimate.entity.Company;
-
-public interface CrudRepository extends JpaRepository<Company, Integer>{
-
-}
