62b00afb91f09d39e684a335b521acf414f74fb2
Ardakesbatyr <87215511+Ardakesbatyr@users.noreply.github.com>
2022-06-02 17:37:39 +0600

    Delete Category2Repository.java

--- src/main/java/kz/projectestimate/repository/Category2Repository.java
+++
@@ -1,8 +0,0 @@
-package kz.projectestimate.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import kz.projectestimate.entity.Category2;
-
-public interface Category2Repository extends JpaRepository<Category2, Integer>{
-
-}
