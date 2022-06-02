def08a3016660b8dd604e127c0189d0f49a8a7b8
Ardakesbatyr <87215511+Ardakesbatyr@users.noreply.github.com>
2022-06-02 17:37:44 +0600

    Delete CategoryRepository.java

--- src/main/java/kz/projectestimate/repository/CategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package kz.projectestimate.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import kz.projectestimate.entity.Category;
-
-public interface CategoryRepository extends JpaRepository<Category, Integer>{
-
-}
